b32b9369350a560444f70492be574026a5343bd7
Arthur Nunes <contact@otaldonunes.agency>
2024-02-06 23:25:22 -0300

    fix: remove drizzle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
-import { SmoothScroll } from "@/components/SmoothScroll";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,11 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={inter.className}>
-        <SmoothScroll>
-          <Providers>
+        <Providers>
             {children}
-          </Providers>
-        </SmoothScroll>
+        </Providers>
       </body>
     </html>
   );
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { SmoothScroll } from '@/components/SmoothScroll'
+import { SmoothScroll } from "@/components/SmoothScroll";
 import { NextUIProvider } from '@nextui-org/react'
 
 export function Providers({children}: { children: React.ReactNode }) {
