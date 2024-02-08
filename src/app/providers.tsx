7cdc589bd77929d3e6fb079a92a408061e3de889
Arthur Nunes <contact@otaldonunes.agency>
2024-02-08 18:27:26 -0300

    feat: i18n

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,8 @@
 'use client'
-import { GTMProvider } from "@/components/GoogleTagManager";
-import { ProgressBar } from "@/components/ProgressBar";
-import { SmoothScroll } from "@/components/SmoothScroll";
+import { GTMProvider } from "@/lib/google/gtm/GoogleTagManager";
+import { ProgressBar } from "@/lib/progressBar/ProgressBar";
+import { SmoothScroll } from "@/lib/smoothScroll/SmoothScroll";
+import LanguageProvider from "@/lib/i18n/LanguageProvider";
 import { NextUIProvider } from '@nextui-org/react'
 import { useRouter } from 'next/navigation'
 
@@ -10,11 +11,13 @@ export function Providers({children}: { children: React.ReactNode }) {
 
   return (
     <SmoothScroll>
-      <NextUIProvider navigate={router.push}>
-        <ProgressBar showSpinner={false} />
-        {children}
-        <GTMProvider gtmId={process.env.GTM_ID || "GTM-ID"} />
-      </NextUIProvider>
+      <LanguageProvider>
+        <NextUIProvider navigate={router.push}>
+          <ProgressBar showSpinner={false} />
+          {children}
+          <GTMProvider gtmId={process.env.GTM_ID || "GTM-ID"} />
+        </NextUIProvider>
+      </LanguageProvider>
     </SmoothScroll>
   )
 }
